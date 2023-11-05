ccca95f85d23d0798251107f033af07b07ca7a2f
EepSAx <113815545+EepSAx@users.noreply.github.com>
2023-11-05 10:37:23 +0700

    Add files via upload

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -2,7 +2,7 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="https://www.instagram.com/sisfor.gac03r/?hl=id">
+			<a href="https://sites.google.com/view/sisforgac003r/">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
